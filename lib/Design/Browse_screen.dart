e50b45cff5468b72525e3aa31cdbed9ddccec5a8
omarbastawy <70014277+omarbastawy@users.noreply.github.com>
2021-12-07 21:03:45 +0200

    movies

---
+++ lib/Design/Browse_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Browse_screen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/Design/Search_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Search_sceen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
